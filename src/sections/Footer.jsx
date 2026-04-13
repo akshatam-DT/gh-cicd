57922dd4ad1b5821d525c51dc70920eb91161d6d
akshatam-DT <akshataam@datatemplate.com>
2026-04-13 16:02:30 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
               Privacy Policy
             </p>
             <p className="text-p5 transition-all duration-500 hover:text-p1">
-              Terms of Usewwwwwwwwwwwwwww
+              Terms of Usewrrrrrr
             </p>
           </div>
 
