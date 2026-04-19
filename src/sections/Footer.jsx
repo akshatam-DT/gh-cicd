e54c4912e15993fe77e5a2e33548e7aa011b2f89
akshatam-DT <akshataam@datatemplate.com>
2026-04-19 14:57:12 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           </div>
           <div className="flex items-center justify-center sm:ml-auto">
             <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
-              Privacy Polllddddd
+              Privacy PollldddddSSSSSSSs
             </p>
             <p className="text-p5 transition-all duration-500 hover:text-p1">
               Terms of Use
